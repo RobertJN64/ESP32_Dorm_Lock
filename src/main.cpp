b5cb0f1cb80b59eec180b89f0cf366c55dbad6ea
RobertJN64 <robertjnies@gmail.com>
2024-09-07 00:38:50 -0400

    detecting board with lib, i2c scan code

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,39 @@
 #include <Arduino.h>
-#include <Wire.h>
+#include <Adafruit_PN532.h>
+
+// TODO - deal with this
+#define PN532_IRQ (2)
+#define PN532_RESET (3) // Not connected by default on the NFC Shield
+
+Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
+
+void fatal_error(const char *err_text) {
+  Serial.print(err_text);
+  Serial.print(" - restarting");
+  delay(5000);
+  ESP.restart();
+}
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  Wire.begin();
   Serial.println("EPS32 connected!");
+
+  nfc.begin();
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (!versiondata) {
+    fatal_error("Didn't find PN53x board");
+  }
+  // Got ok data, print it out!
+  Serial.print("Found chip PN5");
+  Serial.println((versiondata >> 24) & 0xFF, HEX);
+  Serial.print("Firmware ver: ");
+  Serial.print((versiondata >> 16) & 0xFF, DEC);
+  Serial.print('.');
+  Serial.println((versiondata >> 8) & 0xFF, DEC);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  byte error, address;
-  int nDevices;
-  Serial.println("Scanning...");
-  nDevices = 0;
-  for (address = 1; address < 127; address++) {
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-    if (error == 0) {
-      Serial.print("I2C device found at address 0x");
-      if (address < 16) {
-        Serial.print("0");
-      }
-      Serial.println(address, HEX);
-      nDevices++;
-    } else if (error == 4) {
-      Serial.print("Unknow error at address 0x");
-      if (address < 16) {
-        Serial.print("0");
-      }
-      Serial.println(address, HEX);
-    }
-  }
-  if (nDevices == 0) {
-    Serial.println("No I2C devices found\n");
-  } else {
-    Serial.println("done\n");
-  }
-  delay(5000);
 }
